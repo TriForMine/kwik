9996b23e9317baa7ba02f04c5b711482b010c819
TriForMine <quentin.nicolini@hotmail.fr>
2021-09-09 17:42:39 +0200

    Fix formatting

--- mod.ts
+++ mod.ts
@@ -2,5 +2,5 @@ import { msgpack } from "./deps.ts";
 
 export * from "./kwik.ts";
 export * from "./table.ts";
-export const encode = msgpack.encode
-export const decode = msgpack.decode
+export const encode = msgpack.encode;
+export const decode = msgpack.decode;
