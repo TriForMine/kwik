15c036e60d9aef362711c5ad2984d2bac26ff999
TriForMine <quentin.nicolini@hotmail.fr>
2021-09-09 17:13:49 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { fromFileUrl } from "https://deno.land/std@0.103.0/path/mod.ts";
-export { exists } from "https://deno.land/std@0.103.0/fs/exists.ts";
+export { fromFileUrl } from "https://deno.land/std@0.106.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.106.0/fs/exists.ts";
 export * as msgpack from "https://esm.sh/@msgpack/msgpack/mod.ts";
